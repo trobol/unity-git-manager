95aa32b9b2c98f2a3189483908539413ad5c53d7
Thornton <thorfernbacher@gmail.com>
2019-02-04 16:04:26 -0500

    init commit

---
+++ Assets/GitManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+public static class GitManager {
+	public static void ExecuteCommand (string input)
+     {
+		 Command(input);
+		 /* 
+         Thread thread = new Thread(delegate () {Command(input);});
+         thread.Start();
+		 */
+     }
+ 
+	static void Command (string input)
+	{
+		UnityEngine.Debug.Log("Go");
+		ProcessStartInfo processInfo = new ProcessStartInfo("git.exe");
+		processInfo.CreateNoWindow = true;
+		processInfo.UseShellExecute = false;
+		processInfo.RedirectStandardError = true;
+		processInfo.Arguments = input;
+		
+		Process process = Process.Start(processInfo);
+
+		string error;
+		error = process.StandardError.ReadToEnd();
+		process.WaitForExit();
+		process.Close();
+
+		
+
+		UnityEngine.Debug.Log(error);
+		UnityEngine.Debug.Log("Finished");
+		
+	}
+	public static void Fetch() {
+
+	}
+}
+
---
+++ Assets/ShowRemove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ShowRemove : EditorWindow
+{
+    string notification = "This is a Notification";
+
+    [MenuItem("Example/Notification Usage")]
+    static void Initialize()
+    {
+        ShowRemove.GetWindow(typeof(ShowRemove));
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Show Notification"))
+        {
+            this.ShowNotification(new GUIContent(notification));
+        }
+
+        if (GUILayout.Button("Remove Notification"))
+        {
+            this.RemoveNotification();
+        }
+    }
+}
